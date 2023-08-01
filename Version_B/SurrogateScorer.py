Fixed a bug in the calculation of the mean in the process of calculating the "unstability" of the features, specifically it calculated the mean among all candidates, whereas it should have calculate them among the observations.

Version_B/SurrogateScorer.py
@@ -75,7 +75,7 @@ class SurrogateScorer:
 
         standard_deviations = self.variate_model_generator.get_fitness_unstability_scores(feature_presence_matrix,
                                                                                           fitness_array)
-        return 1.0 - (standard_deviations / np.max(standard_deviations))
+        return 1-(standard_deviations / np.max(standard_deviations))
 
     def train(self, candidatesC, fitness_list):
         """this will set the values for S and P"""
@@ -107,7 +107,7 @@ class SurrogateScorer:
 
     def get_surrogate_score_of_fitness(self, candidateC: SearchSpace.Candidate, based_on_trust=False):
         candidate_feature_vector = self.feature_detector.get_feature_presence_from_candidate(candidateC)
-        if (based_on_trust):
+        if based_on_trust:
             candidate_feature_vector *= self.trust_values
         outer_power = utils.nth_power_flat_outer_product(candidate_feature_vector, self.model_power)
         sum_of_fitnesses = np.dot(outer_power, self.S_matrix)

Version_B/VariateModels.py
@@ -97,7 +97,6 @@ class VariateModels:
                                            in chi_squared_and_is_good])
 
         result = (utils.remap_array_in_zero_one(goodness), utils.remap_array_in_zero_one(badness))
-        # TODO result appears to have values outside of 0-1 !!!
         return result
 
     def get_fitness_unfitness_scores(self, feature_presence_matrix, fitness_list) -> (np.ndarray, np.ndarray):
@@ -120,10 +119,13 @@ class VariateModels:
 
     def get_fitness_unstability_scores(self, feature_presence_matrix, fitness_list):
         def standard_deviation(observed_array, presence_array):
-            mean = np.mean(observed_array)
             amount = np.sum(presence_array)
+            if amount < 2:
+                return 0.0
+            mean = np.sum(observed_array)/amount
+            # i have to multiply by the presence array, to only remove the appropriate cases.
             numerator = np.sum(np.square(observed_array - presence_array * mean))
-            standard_deviation = np.sqrt((numerator) / (amount - 1))
+            standard_deviation = np.sqrt(numerator / (amount - 1))
             return standard_deviation / mean
 
 