0190a1bfd46c0513ff4ca9d49366cb0aae283c3f
gian_from_home <g.a.catalano@stir.ac.uk>
2023-09-14 11:52:18 +0100

    Starting the Popularity Criterion

--- Version_D/MeasurableCriterion.py
+++ Version_D/MeasurableCriterion.py
@@ -2,8 +2,10 @@ import Feature
 import numpy as np
 from typing import Iterable, Any
 
+import SearchSpace
 import utils
 from Version_D.PrecomputedFeatureInformation import PrecomputedFeatureInformation
+from SearchSpace import SearchSpace
 
 
 def remap_array_in_zero_one(input: np.ndarray):
@@ -74,3 +76,28 @@ class FitnessConsistencyCriterion(MeasurableCriterion):
 
     def get_raw_score_array(self, pfi: PrecomputedFeatureInformation) -> np.ndarray:
         return self.compute_t_scores(pfi)
+
+class PopularityCriterion(MeasurableCriterion):
+    marginal_probabilities: tuple[tuple[float]] # marginal probability for each (var, val)
+
+
+    def compute_uniform_distribution_marginal_probabilities(self, search_space: SearchSpace) -> tuple[tuple[float]]:
+        def compute_uniform_distribution_of_variable(cardinality) -> tuple[float]:
+            probability = 1 / cardinality
+            return tuple([probability] * cardinality)
+
+        return tuple(compute_uniform_distribution_of_variable(cardinality)
+                     for cardinality in search_space.cardinalities)
+
+
+    def compute_observed_distribution_marginal_probabilities(self, search_space: SearchSpace, pfi: PrecomputedFeatureInformation) -> tuple[tuple[float]]:
+        sum_in_hot_encoding_order: list[float] = np.sum(self.candidate_matrix, axis=0).tolist()
+
+        def counts_for_each_variable(var_index):
+            start, end = self.search_space.precomputed_offsets[var_index: var_index + 2]
+            return sum_in_hot_encoding_order[start:end]
+
+        return [counts_for_each_variable(var_index) for var_index in range(self.search_space.dimensions)]
+
+    def __init__(self, relative_to_uniform_distribution = True,
+                       relative_to_observed_marginals = False):
