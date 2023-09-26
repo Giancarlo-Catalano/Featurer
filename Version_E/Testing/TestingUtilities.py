0d20aa1237b97667b2831e600e185efd12826559
Giancarlo_Catalano <g.a.catalano@stir.ac.uk>
2023-09-26 14:56:19 +0100

    Output json is put into an appropriately named file

--- Version_E/Testing/TestingUtilities.py
+++ Version_E/Testing/TestingUtilities.py
@@ -1,40 +1,41 @@
-import struct
-from enum import Enum, auto
-from typing import Callable
+import json
+import time
+from json import JSONDecodeError
 
-from BenchmarkProblems.CombinatorialProblem import CombinatorialProblem
-from BenchmarkProblems.BinVal import BinValProblem
-from BenchmarkProblems.OneMax import OneMaxProblem
 from BenchmarkProblems.ArtificialProblem import ArtificialProblem
-from BenchmarkProblems.Knapsack import KnapsackProblem
-from BenchmarkProblems.GraphColouring import GraphColouringProblem
+from BenchmarkProblems.BinVal import BinValProblem
 from BenchmarkProblems.CheckerBoard import CheckerBoardProblem
+from BenchmarkProblems.CombinatorialProblem import CombinatorialProblem
+from BenchmarkProblems.GraphColouring import GraphColouringProblem
+from BenchmarkProblems.Knapsack import KnapsackProblem
+from BenchmarkProblems.OneMax import OneMaxProblem
 from BenchmarkProblems.TrapK import TrapK
-import utils
+from SearchSpace import Candidate
+from Version_E.BaselineAlgorithms import RandomSearch, HillClimber
+from Version_E.BaselineAlgorithms.GA import GAMiner
+from Version_E.BaselineAlgorithms.HillClimber import HillClimber
+from Version_E.BaselineAlgorithms.RandomSearch import RandomSearch
 from Version_E.InterestingAlgorithms.ConstructiveMiner import ConstructiveMiner
 from Version_E.InterestingAlgorithms.DestructiveMiner import DestructiveMiner
-from Version_E.BaselineAlgorithms.GA import GAMiner
-from Version_E.BaselineAlgorithms.RandomSearch import RandomSearch
-from Version_E.BaselineAlgorithms.HillClimber import HillClimber
-from Version_E.BaselineAlgorithms import GA, RandomSearch, HillClimber
 from Version_E.InterestingAlgorithms.Miner import FeatureMiner
-from Version_E.MeasurableCriterion.CriterionUtilities import All, Any, Not, Balance
-from Version_E.MeasurableCriterion.GoodFitness import HighFitness, ConsistentFitness
-from Version_E.MeasurableCriterion.Explainability import Explainability
-from Version_E.MeasurableCriterion.Popularity import Overrepresentation, Commonality
 from Version_E.InterestingAlgorithms.Miner import FeatureSelector
+from Version_E.MeasurableCriterion.CriterionUtilities import All, Any, Not, Balance
+from Version_E.MeasurableCriterion.Explainability import Explainability
+from Version_E.MeasurableCriterion.GoodFitness import HighFitness, ConsistentFitness
+from Version_E.MeasurableCriterion.Popularity import Overrepresentation, Commonality
 from Version_E.MeasurableCriterion.Robustness import Robustness
+from Version_E.PrecomputedPopulationInformation import PrecomputedPopulationInformation
 
-import json
+JSON = dict
 
 from Version_E.MeasurableCriterion.MeasurableCriterion import MeasurableCriterion
 
 
-def to_json_object(input) -> dict:
+def to_json_object(input) -> JSON:
     return json.loads(input)
 
 
-def decode_problem(properties: dict) -> CombinatorialProblem:
+def decode_problem(properties: JSON) -> CombinatorialProblem:
     problem_string = properties["problem"]
 
     if problem_string == "binval":
@@ -47,7 +48,7 @@ def decode_problem(properties: dict) -> CombinatorialProblem:
                      k=properties["k"])
     elif problem_string == "checkerboard":
         return CheckerBoardProblem(rows=properties["rows"],
-                                   cols = properties["cols"])
+                                   cols=properties["cols"])
     elif problem_string == "artificial":
         return ArtificialProblem(amount_of_bits=properties["size"],
                                  size_of_partials=properties["size_of_partials"],
@@ -64,7 +65,8 @@ def decode_problem(properties: dict) -> CombinatorialProblem:
     else:
         raise Exception("The problem was not recognised")
 
-def decode_criterion(properties: dict, problem: CombinatorialProblem) -> MeasurableCriterion:
+
+def decode_criterion(properties: JSON, problem: CombinatorialProblem) -> MeasurableCriterion:
     criterion_string = properties["criterion"]
 
     if criterion_string == "explainability":
@@ -82,7 +84,7 @@ def decode_criterion(properties: dict, problem: CombinatorialProblem) -> Measura
     elif criterion_string == "commonality":
         return Commonality()
     elif criterion_string == "robustness":
-        return Robustness(properties["min_diff"],"max_diff")
+        return Robustness(properties["min_diff"], "max_diff")
     elif criterion_string == "not":
         return Not(decode_criterion(properties["argument"], problem))
     elif criterion_string == "all":
@@ -97,23 +99,22 @@ def decode_criterion(properties: dict, problem: CombinatorialProblem) -> Measura
         return Balance(parsed_arguments, weights=weights)
 
 
-def decode_algorithm(properties: dict, selector: FeatureSelector) -> FeatureMiner:
+def decode_miner(properties: JSON, selector: FeatureSelector) -> FeatureMiner:
     """ converts a json string into an instance of a FeatureMiner object"""
 
     kind = properties["kind"]
     population_size = properties["popsize"]
 
-
     if kind in "Constructive":
         return ConstructiveMiner(selector,
                                  stochastic=properties["stochastic"],
-                                 at_most_parameters = properties["at_most"],
-                                 amount_to_keep_in_each_layer= properties["amount_to_keep_in_each_layer"])
-    elif kind == "Destructive":
-        return  DestructiveMiner(selector,
-                                 stochastic=properties["stochastic"],
-                                 at_least_parameters=properties["at_least"],
+                                 at_most_parameters=properties["at_most"],
                                  amount_to_keep_in_each_layer=properties["amount_to_keep_in_each_layer"])
+    elif kind == "Destructive":
+        return DestructiveMiner(selector,
+                                stochastic=properties["stochastic"],
+                                at_least_parameters=properties["at_least"],
+                                amount_to_keep_in_each_layer=properties["amount_to_keep_in_each_layer"])
     elif kind == "GA":
         return GAMiner(selector,
                        population_size=population_size,
@@ -125,13 +126,90 @@ def decode_algorithm(properties: dict, selector: FeatureSelector) -> FeatureMine
         return RandomSearch(selector,
                             amount_to_generate=population_size)
 
-class TestCase(Enum):
-    COUNT_IDEALS = auto()
-    CHECK_DISTRIBUTION = auto()
 
-def decode_test(properties: str):
-    test_string = properties["test"]
-    if test_string == "count_ideals":
-        return TestCase.COUNT_IDEALS
-    elif test_string == "check_distribution":
-        return TestCase.CHECK_DISTRIBUTION
+def get_random_candidates_and_fitnesses(problem: CombinatorialProblem,
+                                        sample_size) -> (list[Candidate], list[float]):
+    random_candidates = [problem.get_random_candidate_solution() for _ in range(sample_size)]
+
+    scores = [problem.score_of_candidate(c) for c in random_candidates]
+    return random_candidates, scores
+
+
+def get_training_data(problem: CombinatorialProblem,
+                      sample_size) -> PrecomputedPopulationInformation:
+    training_samples = [problem.get_random_candidate_solution() for _ in range(sample_size)]
+    fitness_list = [problem.score_of_candidate(c) for c in training_samples]
+    return PrecomputedPopulationInformation(problem.search_space, training_samples, fitness_list)
+
+
+def error_result(description) -> JSON:
+    return {"error": description}
+
+
+Seconds = float
+
+
+def timed_function(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        return result, execution_time
+
+    return wrapper
+
+
+@timed_function
+def count_ideals_test(problem, miner):
+    pass
+
+
+@timed_function
+def check_distribution_test(problem, miner):
+    pass
+
+
+def apply_test(test_type: str, problem: CombinatorialProblem, miner: FeatureMiner) -> JSON:
+    if test_type == "count_ideals":
+        test_type_function = count_ideals_test
+    elif test_type == "check_distribution":
+        test_type_function = check_distribution_test
+    else:
+        return error_result("invalid test")
+
+    data, execution_time = test_type_function(problem, miner)
+
+    data["runtime"] = execution_time
+    return data
+
+
+def run_test(arguments_string) -> JSON:
+    arguments_json = to_json_object(to_json_object(arguments_string))
+    try:
+        problem_object = decode_problem(arguments_json["problem"])
+        criterion_object = decode_criterion(arguments_json["criterion"], problem_object)
+    except KeyError:
+        return error_result("missing parameter from JSON in problem settings")
+    except JSONDecodeError:
+        return error_result("JSON could not be parsed in problem settings")
+
+    try:
+        test_type = arguments_json["test"]
+        sample_size = arguments_json["sample_size"]
+        training_ppi = get_training_data(problem_object, sample_size)
+        selector = FeatureSelector(training_ppi, criterion_object)
+        miner_object = decode_miner(arguments_json["miner"], selector)
+    except KeyError:
+        return error_result("missing parameter from JSON in test settings")
+    except JSONDecodeError:
+        return error_result("JSON could not be parsed in test settings")
+
+    result_json = apply_test(test_type, problem_object, miner_object)
+
+    output_name = f"{test_type}~{sample_size}~{problem_object}~{miner_object}"
+
+    output_json = {"parameters": arguments_json, "result": result_json}
+
+    with open(output_name, 'w') as json_file:
+        json.dump(output_json, json_file)
