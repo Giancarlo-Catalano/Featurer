4dc449d6aecb1d3dda9378c4e9d680a2c3764627
Giancarlo_Catalano <g.a.catalano@stir.ac.uk>
2023-11-06 17:03:16 +0000

    Implemented the Plateau Problem.

---
+++ BenchmarkProblems/PlateauProblem.py
@@ -0,0 +1,42 @@
+import SearchSpace
+import utils
+from BenchmarkProblems.CombinatorialProblem import TestableCombinatorialProblem
+from Version_E.Feature import Feature
+
+
+class TrapK(TestableCombinatorialProblem):
+    amount_of_groups: int
+    k = 3
+
+    def __init__(self, amount_of_groups):
+        self.amount_of_groups = amount_of_groups
+        amount_of_bits = self.k * self.amount_of_groups
+        super().__init__(SearchSpace.SearchSpace([2] * amount_of_bits))
+
+    def __repr__(self):
+        return f"Plateau({self.k}, {self.amount_of_groups})"
+
+    def long_repr(self):
+        return f"Plateau(K={self.k}, amount of groups = {self.amount_of_groups})"
+
+    def get_complexity_of_feature(self, feature: SearchSpace.UserFeature):
+        return super().amount_of_set_values_in_feature(feature)
+
+    def score_of_candidate(self, candidate: SearchSpace.Candidate):
+
+        def score_for_group(group_index):
+            return int(all(candidate.values[i] for i in range(3*group_index, 3*(group_index+1))))
+
+        return sum(score_for_group(group_index) for group_index in range(self.amount_of_groups))
+
+
+    def feature_repr(self, feature):
+        return f"{feature}"
+
+    def get_ideal_features(self) -> list[Feature]:
+
+        def get_ideal_for_group(group_index):
+            var_vals = [(var, 1) for var in range(3*group_index, 3*(group_index+1))]
+            return Feature.from_legacy_feature(SearchSpace.UserFeature(var_vals), self.search_space)
+
+        return [get_ideal_for_group(group_index) for group_index in range(self.amount_of_groups)]
